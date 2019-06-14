5a1b7c23c9a04e4e6f787b5d086658d4d0938cb2
sandro888 <36017398+sandro888@users.noreply.github.com>
2019-06-14 22:21:05 +0400

    first

---
+++ Final/public/main.js
@@ -0,0 +1,62 @@
+const appKey = "178697fb05cc43a775e961d3ae6584fe";
+
+let searchButton = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city-name");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temp");
+let humidity = document.getElementById("humidity-div");
+
+let alo = document.getElementsByTagName('body')
+var image =
+searchButton.addEventListener("click", findWeatherDetails);
+searchInput.addEventListener("keyup", enterPressed);
+
+function enterPressed(event) {
+  if (event.key === "Enter") {
+    findWeatherDetails();
+  }
+}
+
+function findWeatherDetails() {
+  if (searchInput.value === "") {
+  
+  }else {
+    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
+   httpRequestAsync(searchLink, theResponse);
+  }
+ }
+
+function theResponse(response) {
+  let jsonObject = JSON.parse(response);
+  cityName.innerHTML = jsonObject.name;
+  icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+  temperature.innerHTML = parseInt(jsonObject.main.temp - 273)  ;
+  
+  humidity.innerHTML = "Hum." + jsonObject.main.humidity + "%";
+  console.log(temperature.innerHTML)
+  if(temperature.innerHTML < 20){
+    document.body.classList.add('cloudy')
+    document.body.classList.remove('sunny')
+
+
+  }
+  else if(temperature.innerHTML >= 20){
+    document.body.classList.add('sunny')
+    document.body.classList.remove('cloudy')
+
+
+  }
+}
+
+function httpRequestAsync(url, callback)
+{
+  console.log("hello");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => { 
+        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true); // true for asynchronous 
+    httpRequest.send();
+}
